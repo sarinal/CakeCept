6b5b393e0e2f97c8239986de1843a59b009f53dc
sarinal <sarina-luu@hotmail.com>
2018-12-02 00:02:21 -0500

    Designer Functionalities

--- app/src/main/java/com/example/sarii/cakecept/CustomView.java
+++ app/src/main/java/com/example/sarii/cakecept/CustomView.java
@@ -3,11 +3,15 @@ package com.example.sarii.cakecept;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.TypedValue;
 
 import org.w3c.dom.Attr;
 //import java.nio.file.Path;
@@ -15,76 +19,67 @@ import org.w3c.dom.Attr;
 
 public class CustomView extends View {
 
-//    private Context context;
-//    private AttributeSet attrs;
+    private Context context;
+    private AttributeSet attrs;
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+    private float brushSize, lastBrushSize;
+    private boolean erase=false;
 
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        setupDrawing();
     }
+    private void setupDrawing(){
+//get drawing area setup for interaction
 
-    //drawing path
-    private Path drawPath;
-
-    //defines what to draw
-    private Paint canvasPaint;
-
-    //defines how to draw
-    private Paint drawPaint;
-
-    //initial color
-    private int paintColor = 0xFF660000;
-
-    //canvas - holding pen, holds your drawings
-    //and transfers them to the view
-    private Canvas drawCanvas;
-
-    //canvas bitmap
-    private Bitmap canvasBitmap;
-
-    //brush size
-    private float currentBrushSize, lastBrushSize;
-
-    private void init(){
-        currentBrushSize = getResources().getInteger(R.integer.medium_size);
-        lastBrushSize = currentBrushSize;
-
+        brushSize = getResources().getInteger(R.integer.medium_size);
+        lastBrushSize = brushSize;
         drawPath = new Path();
         drawPaint = new Paint();
+
         drawPaint.setColor(paintColor);
         drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(currentBrushSize);
+
+        drawPaint.setStrokeWidth(brushSize);
         drawPaint.setStyle(Paint.Style.STROKE);
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
-
         canvasPaint = new Paint(Paint.DITHER_FLAG);
+
 
     }
 
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(canvasBitmap, 0 , 0, canvasPaint);
-        canvas.drawPath(drawPath, drawPaint);
-}
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        //create canvas of certain device size.
+    //view given size
         super.onSizeChanged(w, h, oldw, oldh);
-
-        //create Bitmap of certain w,h
         canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-
-        //apply bitmap to graphic to start drawing.
         drawCanvas = new Canvas(canvasBitmap);
     }
 
     @Override
+    protected void onDraw(Canvas canvas) {
+    //draw view
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+
+    }
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
+        //detect user touch
         float touchX = event.getX();
         float touchY = event.getY();
-        //respond to down, move and up events
+
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 drawPath.moveTo(touchX, touchY);
@@ -93,17 +88,51 @@ public class CustomView extends View {
                 drawPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
-                drawPath.lineTo(touchX, touchY);
                 drawCanvas.drawPath(drawPath, drawPaint);
                 drawPath.reset();
                 break;
             default:
                 return false;
         }
-        //redraw
+
         invalidate();
         return true;
     }
+
+
+    public void setColor(String newColor){
+    //set color
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+
+    public void setBrushSize(float newSize){
+//update size
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                newSize, getResources().getDisplayMetrics());
+        brushSize=pixelAmount;
+        drawPaint.setStrokeWidth(brushSize);
+    }
+
+    public void setLastBrushSize(float lastSize){
+        lastBrushSize=lastSize;
+    }
+    public float getLastBrushSize(){
+        return lastBrushSize;
+    }
+
+    public void setErase(boolean isErase){
+    //set erase true or false
+        erase=isErase;
+        if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        else drawPaint.setXfermode(null);
+
+    }
+
+
+
+
 }
 
 
--- app/src/main/java/com/example/sarii/cakecept/Designer.java
+++ app/src/main/java/com/example/sarii/cakecept/Designer.java
@@ -1,28 +1,203 @@
 package com.example.sarii.cakecept;
 
 
-import android.support.v7.app.ActionBar;
-import android.content.Intent;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import java.util.UUID;
+import android.provider.MediaStore;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class Designer extends AppCompatActivity {
+
+public class Designer extends AppCompatActivity implements OnClickListener {
 
     private Toolbar mToolbar;
+    //boolean flag to know if main FAB is in open or closed state.
+
+    private CustomView drawView;
+    private ImageButton currPaint,drawBtn, eraseBtn, saveBtn;
+    private float smallBrush, mediumBrush, largeBrush;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_designer);
 
-        mToolbar = (Toolbar) findViewById(R.id.action_bar);
+//        mToolbar = (Toolbar) findViewById(R.id.action_bar);
+//
+//        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setHomeButtonEnabled(true);
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
+        drawBtn = (ImageButton)findViewById(R.id.btn_brush);
+        drawBtn.setOnClickListener(this);
+        eraseBtn = (ImageButton)findViewById(R.id.btn_eraser);
+        eraseBtn.setOnClickListener(this);
+        saveBtn = (ImageButton)findViewById(R.id.btn_save);
+        saveBtn.setOnClickListener(this);
 
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+        drawView = (CustomView) findViewById(R.id.drawing);
+        drawView.setBrushSize(mediumBrush);
+
+
     }
+
+    public void paintClicked(View view){
+        //use chosen color
+
+        drawView.setErase(false);
+        drawView.setBrushSize(drawView.getLastBrushSize());
+
+        if(view!=currPaint){
+        //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
+
+
+    }
+    @Override
+    public void onClick(View view){
+    //respond to clicks
+        if(view.getId()==R.id.btn_brush){
+            //draw button clicked
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Brush size:");
+
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(smallBrush);
+                    drawView.setLastBrushSize(smallBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(mediumBrush);
+                    drawView.setLastBrushSize(mediumBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(largeBrush);
+                    drawView.setLastBrushSize(largeBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+                }
+            });
+
+            brushDialog.show();
+        }
+        else if(view.getId()==R.id.btn_eraser){
+            //switch to erase - choose size
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Eraser size:");
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setErase(true);
+                    drawView.setBrushSize(smallBrush);
+                    brushDialog.dismiss();
+                }
+            });
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setErase(true);
+                    drawView.setBrushSize(mediumBrush);
+                    brushDialog.dismiss();
+                }
+            });
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setErase(true);
+                    drawView.setBrushSize(largeBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            brushDialog.show();
+
+
+        }
+
+        else if(view.getId()==R.id.btn_save){
+            //save drawing
+            AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+            saveDialog.setTitle("Save drawing");
+            saveDialog.setMessage("Save drawing to device Gallery?");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    //save drawing
+                    drawView.setDrawingCacheEnabled(true);
+                    String imgSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), drawView.getDrawingCache(),
+                            UUID.randomUUID().toString()+".png", "drawing");
+
+                    if(imgSaved!=null){
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    }
+                    else{
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+
+                }
+            });
+            saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            saveDialog.show();
+
+            drawView.destroyDrawingCache();
+
+        }
+
+
+    }
+
+
 
 
 
